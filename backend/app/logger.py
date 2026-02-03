a34d0e019469eb5cc8256b591238fd13c3ba91ac
SchadenKai <kairustecsonpersonal@gmail.com>
2026-02-03 09:25:22 +0800

    fix: set logging level to INFO for consistent logging behavior

--- backend/app/logger.py
+++ backend/app/logger.py
@@ -1,10 +1,9 @@
 import logging
 from logging import StreamHandler
 
-from app.core.config import settings
 
 logging.basicConfig(
-    level=logging.DEBUG if settings.dev_mode else logging.INFO,
+    level=logging.INFO,
     format=(
         "[%(asctime)s][%(levelname)s] %(module)s:%(funcName)s:%(lineno)d"
         "             %(message)s"
