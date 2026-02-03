785912b057bb1ff675b58e5c4c9a1f21582dfc64
SchadenKai <kairustecsonpersonal@gmail.com>
2026-02-03 08:00:49 +0800

    feat: enhance lifespan management with conditional initialization for chat model and embedding services

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,14 +2,13 @@ from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from typing import Annotated
 
+import torch
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.logger import app_logger
 from app.rag.db import VectorClient
-from app.routes.dependencies.embedding import get_embedding
-from app.routes.dependencies.llm import get_chat_model_service
 from app.routes.dependencies.settings import get_app_settings
 from app.routes.dependencies.vector_db import (
     get_vector_client,
@@ -19,22 +18,26 @@ from app.routes.v1.main import v1_router
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator:
+async def lifespan(_: FastAPI) -> AsyncGenerator:
     app_logger.info("Initializing vector database")
     settings = get_app_settings()
     vector_db = get_vector_client_manual(settings)
     # vector_db.delete_collection()
     vector_db.setup()
     vector_db.load_collection()
-    vector_db.smoke_test()
+    if settings.dev_mode:
+        from app.routes.dependencies.embedding import get_embedding
+        from app.routes.dependencies.llm import get_chat_model_service
 
-    app_logger.info("Initializing chat model")
-    chat_model_service = get_chat_model_service(settings)
-    chat_model_service.test_chat_model()
+        vector_db.smoke_test()
 
-    app_logger.info("Initializing embedding services")
-    embedding_service = get_embedding(settings)
-    embedding_service.test_client_on_startup()
+        app_logger.info("Initializing chat model")
+        chat_model_service = get_chat_model_service(settings)
+        chat_model_service.test_chat_model()
+
+        app_logger.info("Initializing embedding services")
+        embedding_service = get_embedding(settings)
+        embedding_service.test_client_on_startup()
 
     app_logger.info("Verifying Pytorch CUDA Installation")
     app_logger.info(f"Pytorch version: {torch.__version__}")
