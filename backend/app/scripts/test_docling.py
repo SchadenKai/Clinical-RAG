d392621a607fbd197b989120f7cf5a1b464d540d
SchadenKai <kairustecsonpersonal@gmail.com>
2026-03-03 18:43:10 +0800

    feat: enhance document processing in test_docling.py; refactor chunking and output handling

--- backend/app/scripts/test_docling.py
+++ backend/app/scripts/test_docling.py
@@ -1,27 +1,47 @@
-from docling.chunking import HierarchicalChunker
-from docling.document_converter import DocumentConverter
-from docling.datamodel.pipeline_options import PdfPipelineOptions, tbale
+import json
+import os
+
+os.environ["TORCH_COMPILE_DISABLE"] = "1"
+from docling.chunking import HierarchicalChunker, HybridChunker
 
 from app.routes.dependencies.file_store import get_s3_service
 from app.routes.dependencies.settings import get_app_settings
+from app.routes.dependencies.tokenizer import get_tokenizer_service
+from app.services.document_converter import doc_converter
 from app.services.file_store.context_manager import S3FileStager
 
-_FILE_NAME = "9789240115774-eng.pdf"
-
-doc_converter = DocumentConverter()
+_FILE_NAME = "Galaxy - Wikipedia.pdf"
+MD_FILE_DEST = "test_md_1.md"
+HIERARCHAL_RESULTS_DEST = "hierarchal_chunking.json"
+HYBRID_RESULTS_DEST = "hybrid_chunking_batch_1.json"
+DOCLING_DOCUMENT_DEST = "docling_document.json"
 
 app_settings = get_app_settings()
 s3_service = get_s3_service(app_settings)
+tokenizer_service = get_tokenizer_service(app_settings)
 
 with S3FileStager(
     s3_service=s3_service, file_key=_FILE_NAME, settings=app_settings
 ) as file_path:
-    doc = doc_converter.convert(file_path)
-    doc = doc.document
-    chunks = doc.texts
-    new_chunks = []
-    print(f"Initial number of chunks: {len(chunks)}")
-    chunker = HierarchicalChunker(always_emit_headings=True, merge_list_items=True)
-    chunks = chunker.chunk(doc)
-    chunks = list(chunks)
-    print(f"Number of chunks after hierachal chunking: {len(chunks)}")
+    doc = doc_converter(file_path)
+    with open(file=DOCLING_DOCUMENT_DEST, mode="w", encoding="utf-8") as f:
+        json.dump(doc, f, indent=4, ensure_ascii=False)
+    
+    md_text = doc.export_to_markdown()
+    with open(file=MD_FILE_DEST, mode="w", encoding="utf-8") as f:
+        f.write(md_text)
+
+    hierarchal_chunker = HierarchicalChunker(
+        always_emit_headings=True, merge_list_items=True
+    )
+    chunks = hierarchal_chunker.chunk(doc)
+    serializable_chunks = [chunk.model_dump() for chunk in chunks]
+    with open(file=HIERARCHAL_RESULTS_DEST, mode="w", encoding="utf-8") as f:
+        json.dump(serializable_chunks, f, indent=4, ensure_ascii=False)
+
+    tokenizer = tokenizer_service.embedding_tokenizer
+    hybrid_chunker = HybridChunker(tokenizer=tokenizer, max_tokens=512)
+    chunks_1 = hybrid_chunker.chunk(doc)
+    serializable_chunks = [chunk.model_dump() for chunk in chunks_1]
+    with open(file=HYBRID_RESULTS_DEST, mode="w", encoding="utf-8") as f:
+        json.dump(serializable_chunks, f, indent=4, ensure_ascii=False)
